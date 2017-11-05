130ab7cc2164aeb1ad68a4361e624f7157702199
Rafael A. Irizarry <rairizarry@gmail.com>
2017-11-05 17:19:02 -0500

    adding two chapters and the webpage

---
+++ _common.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+library(dslabs)
+ds_theme_set()
+
+## Copied from Hadley Wickham and Garrett Grolemund and Hadley Wickham's r4ds
+options(digits = 3)
+
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  cache = TRUE,
+  out.width = "70%",
+  fig.align = 'center',
+  fig.width = 6,
+  fig.height = 3.708,  # width * 1 / phi
+  fig.show = "hold"
+)
+
+options(dplyr.print_min = 6, dplyr.print_max = 6)
