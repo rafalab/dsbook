5e50bac8d657ef837f92f93c5f9b58f1c2d4a803
Rafael A. Irizarry <rairizarry@gmail.com>
2019-08-30 09:59:08 -0400

    minor changes

--- _common.R
+++ _common.R
@@ -6,18 +6,20 @@ library(tidyverse)
 library(dslabs)
 ds_theme_set()
 
-## Copied from Hadley Wickham and Garrett Grolemund's r4ds
-options(digits = 3)
+## Adapted from Hadley Wickham and Garrett Grolemund's r4ds
+options(digits = 3, width = 72, formatR.indent = 2)
 
 knitr::opts_chunk$set(
   comment = "#>",
   collapse = TRUE,
   cache = TRUE,
+  width = 72,
+  tidy.opts=list(width.cutoff=72, tidy=TRUE),
   out.width = "70%",
   fig.align = 'center',
   fig.width = 6,
   fig.height = 3.708,  # width * 1 / phi
   fig.show = "hold")
 
-options(dplyr.print_min = 6, dplyr.print_max = 6)
+options(dplyr.print_min = 5, dplyr.print_max = 5)
 
